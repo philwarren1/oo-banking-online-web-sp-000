36aba830b3c5fe1c5e0876b47d02490e7634ca4d
Philip Warren <philwarren1@gmail.com>
2020-08-13 01:20:23 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -15,5 +15,15 @@ class BankAccount
     @balance += amount
   end
   
-  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0 ? true : false
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
