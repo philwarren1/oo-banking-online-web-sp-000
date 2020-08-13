8420adac5ec9febb7d8d9696bbeab9c848ed447d
Philip Warren <philwarren1@gmail.com>
2020-08-13 00:52:19 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,11 +2,12 @@ class BankAccount
  
   @@all = []
 
-  attr_accessor :name, :balance
+  attr_accessor :name, :balance, :status
 
   def initialize(name)
         @name = name
         @@all << self
         @balance = 1000
+        @status = "open"
   end
 end
